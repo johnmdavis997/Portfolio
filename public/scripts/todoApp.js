af64263bf9e5b9805beb218202237f8a7be71770
Kai Davis <johnmdavis997@gmail.com>
2017-08-27 22:36:11 -0700

    Added todo list

---
+++ public/scripts/todoApp.js
@@ -0,0 +1,32 @@
+// Check off specific todos by clicking
+$("ul").on("click", "li", function () {
+    $(this).toggleClass("completed");
+})
+
+// Click on X to delete Todo
+$("ul").on("click", "span", function (event) {
+    event.stopPropagation();
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+})
+
+// Add new items when enter key is pressed
+$("input[type='text']").on("keypress", function (event) {
+    if (event.which === 13) {
+        addText();
+    }
+});
+
+// Fades input in and out
+$("button").on("click", function () {
+    $("input").fadeToggle(500);
+});
+
+function addText() {
+    var todoText = $("input").val(); // grabs new todo text from input
+    if ($("input").val() > "") {
+        $("ul").append("<li><span><i class='fa fa-trash' aria-hidden='true'></i></span> " + todoText + "</li>");
+    }
+    $("input").val("");
+}
--- routes/projects.js
+++ routes/projects.js
@@ -10,4 +10,8 @@ router.get("/circles", function(req, res){
     res.render("projects/circles")
 })
 
+router.get("/todo", function(req, res){
+    res.render("projects/todo")
+})
+
 module.exports = router;
